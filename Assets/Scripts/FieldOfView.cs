3ce0cb2d9461839369969f85118c60683fe1f805
MarkoSopta <philoyd99@gmail.com>
2023-08-09 14:27:27 +0200

    Fov and distance to target visualized

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask objectMask;
+
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    void Start() {
+        StartCoroutine("FindTargets", .3f);
+    
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
+    IEnumerator FindTargets(float delay) {
+        while (true) {
+
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    
+    }
+
+
+
+    void FindVisibleTargets() {
+        visibleTargets.Clear();
+
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2) {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, objectMask)) {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    
+    }
+    
+    public Vector3 DirectionFromAngle(float angleInDeg, bool angleIsGlobal) {
+        if (!angleIsGlobal) {
+            angleInDeg += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDeg * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDeg * Mathf.Deg2Rad));
     }
 }
